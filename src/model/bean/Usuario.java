37ee467858a48a9575d444e57056f2bf468832b7
Matheus Rocha <theusrsilva@gmail.com>
2018-11-21 22:55:24 -0200

    connection

--- src/model/bean/Usuario.java
+++ src/model/bean/Usuario.java
@@ -16,4 +16,88 @@ public class Usuario {
     private String telefone;
     private String email;
     private String senha_hash;
+
+    /**
+     * @return the id_usuario
+     */
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    /**
+     * @param id_usuario the id_usuario to set
+     */
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the senha_hash
+     */
+    public String getSenha_hash() {
+        return senha_hash;
+    }
+
+    /**
+     * @param senha_hash the senha_hash to set
+     */
+    public void setSenha_hash(String senha_hash) {
+        this.senha_hash = senha_hash;
+    }
 }
